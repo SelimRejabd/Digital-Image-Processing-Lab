f9c1e0b5d427a159db301163dd21343a35395339
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2023-11-11 23:20:27 +0600

    average and median filter added

--- 3a.py
+++ 3a.py
@@ -29,14 +29,51 @@ add_salt_noise(noise_image)
 add_papper_noise(noise_image)
 
 def calculate_psnr(original_image, noisy_image):
+    original_image = original_image.astype(np.float64)
+    noisy_image = noisy_image.astype(np.float64)
     mse = np.mean((original_image - noisy_image) ** 2)
     max_pixel = 255.0
     psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
     return psnr
 
-average_filtered = cv2.blur(noise_image, (5, 5))
+def average_filter(noisy_image, kernel_size):
+    filtered_image = np.zeros_like(noisy_image)
+    pad_size = kernel_size // 2
+    starting_row = 0+pad_size
+    starting_col = 0+pad_size
+    ending_row = filtered_image.shape[0] - pad_size
+    ending_col = filtered_image.shape[1] - pad_size
+    mask = np.ones((kernel_size, kernel_size)) / (kernel_size**2)
 
-median_filtered = cv2.medianBlur(noise_image, 5)
+    for i in range(starting_row, ending_row):
+        for j in range(starting_col, ending_col):
+            window = noisy_image[i - pad_size:i + pad_size + 1 , j-pad_size:j+pad_size+1]
+            temp_window = window.copy()
+            temp_window = temp_window * mask
+            temp_window_mean = np.sum(temp_window)
+            filtered_image[i][j] = temp_window_mean
+
+    return filtered_image
+
+def median_filter(noisy_image, kernel_size):
+    filtered_image = np.zeros_like(noisy_image)
+    pad_size = kernel_size // 2
+    starting_row = 0+pad_size
+    starting_col = 0+pad_size
+    ending_row = filtered_image.shape[0] - pad_size
+    ending_col = filtered_image.shape[1] - pad_size
+
+    for i in range(starting_row, ending_row):
+        for j in range(starting_col, ending_col):
+            window = noisy_image[i - pad_size:i + pad_size + 1, j-pad_size:j+pad_size+1]
+            window_mean = np.median(window)
+            filtered_image[i][j] = window_mean
+
+    return filtered_image 
+
+average_filtered = average_filter(noise_image, 5)
+
+median_filtered = median_filter(noise_image, 5)
 
 psnr_noise = peak_signal_noise_ratio(image, noise_image)
 psnr_average = peak_signal_noise_ratio(image, average_filtered)
